26392f74d68ba9fd323c54335349e4a11a976d8d
Ryan Raczak <ryanraczak@rocketmortgage.com>
2021-12-01 15:34:16 -0500

    Initial commit

---
+++ GC_MovieRegistration/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GC_MovieRegistration.Models
+{
+    public class Movie
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Year { get; set; }
+        public string Actor { get; set; }
+        public string Director { get; set; }
+    }
+}
